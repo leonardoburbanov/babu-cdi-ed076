6bc58259428a47da863e25ecb553f2875c0749bb
Stackbit Code Editor <projects@stackbit.com>
2021-11-15 21:30:06 +0000

    SectionFeatures.js: changed

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -22,7 +22,7 @@ export default class SectionFeatures extends React.Component {
                     )}
                     <div className="cell block-content">
                         <h3 className="block-title underline">{title}</h3>
-                        <div className="block-copy">{markdownify(content)}</div>
+                        Se
                         {actions && (
                             <div className="block-buttons">
                                 <CtaButtons actions={actions} />
