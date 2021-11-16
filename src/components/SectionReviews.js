c36e7dca904730d4c56f42ad2cffc526f8f5a3dd
Stackbit Code Editor <projects@stackbit.com>
2021-11-16 18:47:38 +0000

    SectionReviews.js: changed

--- src/components/SectionReviews.js
+++ src/components/SectionReviews.js
@@ -18,7 +18,6 @@ export default class SectionReviews extends React.Component {
                     <footer className="review-footer">
                         {avatar && <img className="review-avatar" src={withPrefix(avatar)} alt={avatarAlt} />}
                         {author && <cite className="review-author">{author}</cite>}
-                        {author_desc && <cite className="review-author-desc">{author_desc}</cite>}
                     </footer>
                     <footer className="review-footer">
                         {author_desc && <cite className="review-author-desc">{author_desc}</cite>}
