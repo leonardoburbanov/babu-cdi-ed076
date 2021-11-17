c696c6744083c99d13d0ae9d280d8706e99f5d8d
Stackbit Code Editor <projects@stackbit.com>
2021-11-17 16:53:14 +0000

    SectionCta.js: changed

--- src/components/SectionCta.js
+++ src/components/SectionCta.js
@@ -19,6 +19,7 @@ export default class SectionCta extends React.Component {
                     <div className="grid">
                         <div className="cell block-content">
                             {title && <h2 className="block-title">{title}</h2>}
+                            {subtitle && <p className="block-copy">{htmlToReact(subtitle)}</p>}
                             {markdownify(content)}
                         </div>
                         {actions && (
