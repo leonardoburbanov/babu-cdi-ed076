6a6e8b5a9fb0ed4832acca9361388753c68f971a
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 03:15:49 +0000

    sourcebit.js: changed

--- sourcebit.js
+++ sourcebit.js
@@ -38,6 +38,7 @@ module.exports = {
                     { path: '/{__metadata.urlPath}', predicate: _.matchesProperty('__metadata.modelName', 'landing') },
                     { path: '/{__metadata.urlPath}', predicate: _.matchesProperty('__metadata.modelName', 'page') },
                     { path: '/{__metadata.urlPath}', predicate: _.matchesProperty('__metadata.modelName', 'blog') },
+                    { path: '/{__metadata.urlPath}', predicate: _.matchesProperty('__metadata.modelName', 'post') },
                     { path: '/{__metadata.urlPath}', predicate: _.matchesProperty('__metadata.modelName', 'post') }
                 ],
                 commonProps: {
