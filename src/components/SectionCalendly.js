677c9161ba111b1acf365db4d4a98b96c93a4d72
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 15:54:16 +0000

    SectionCalendly.js: changed

--- src/components/SectionCalendly.js
+++ src/components/SectionCalendly.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
 
-import { htmlToReact,markdownify } from '../utils';
-import CtaButtons from './CtaButtons';
 import Calendly from '../components/Calendly';
 
 
@@ -10,10 +8,6 @@ export default class SectionCalendly extends React.Component {
     render() {
         const section = _.get(this.props, 'section');
         const sectionId = _.get(section, 'section_id');
-        const title = _.get(section, 'title');
-        const subtitle = _.get(section, 'subtitle');
-        const actions = _.get(section, 'actions');
-        const content = _.get(section, 'content');
 
         return (
             <section id={sectionId} className="block cta-block bg-accent outer">
