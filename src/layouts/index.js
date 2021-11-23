d7e4a36730b8bac4b24a164b1ed2dab93bb2a899
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 05:09:36 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import landing from './landing';
 import page from './page';
 import blog from './blog';
 import post from './post';
-import agendar from './agendar';
 
 
 export { landing, page, blog, post, agendar };
