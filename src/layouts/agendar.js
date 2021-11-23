c255747ef6ae45174077a7c7c8a32a8cd4d835e6
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 06:35:49 +0000

    agendar.js: changed

--- src/layouts/agendar.js
+++ src/layouts/agendar.js
@@ -22,7 +22,6 @@ export default class agendar extends React.Component {
                         <article className="post post-full">
                             <header className="post-header">
                                 <h1 className="post-title">{title}</h1>
-                                <h1 className="post-title">{title}</h1>
                                 {subtitle && <div className="post-subtitle">{htmlToReact(subtitle)}</div>}
                             </header>
                             {image && (
