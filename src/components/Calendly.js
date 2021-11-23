5c29e9d3a4f59afe8757627f90b490d79db4d6b6
Stackbit Code Editor <projects@stackbit.com>
2021-11-23 06:44:40 +0000

    Calendly.js: changed

--- src/components/Calendly.js
+++ src/components/Calendly.js
@@ -11,7 +11,7 @@ export default class Calendly extends React.Component {
     return (
       <div>
         <InlineWidget 
-            url="https://calendly.com/leofr7nco/50min"   
+            url="https://calendly.com/leofr7nco/60min"   
             styles={ {
                 height: '1000px'
               }}
